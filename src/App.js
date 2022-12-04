32fa4bd92922aee0eea3c7f5ad2332defc7cb077
justinmilushev <justin@milushev.com>
2022-12-04 15:58:03 -0600

    adding more survey questions and submit button

--- src/App.js
+++ src/App.js
@@ -4,15 +4,50 @@ import ChecklistBox from "./ChecklistBox"
 
 function App() {
   const questions = [
-    {text: "Do you like dogs?", clicked: false},
-    {text: "Do you like cats?", clicked: false}
+    {text: "You like dogs", clicked: false},
+    {text: "You like cats", clicked: false},
+    {text: "You like snakes", clicked: false},
+    {text: "You like farm animals", clicked: false},
+    {text: "You consider yourself intelligent, in terms of book smarts and IQ", clicked: false},
+    {text: "You consider yourself physically strong", clicked: false},
+    {text: "You have a great sense of hearing", clicked: false},
+    {text: "You have really bad eye-sight", clicked: false},
+    {text: "You have siblings", clicked: false},
+    {text: "You believe in the afterlife (i.e. heaven or hell)", clicked: false},
+    {text: "You are a night owl", clicked: false},
+    {text: "You are a fast runner", clicked: false},
+    {text: "You are physically attractive", clicked: false},
+    {text: "You are tall", clicked: false},
+    {text: "You are older than 30", clicked: false},
+    {text: "You frequently fly on airplains", clicked: false},
+    {text: "You have pranked or tricked someone", clicked: false},
+    {text: "You have been to a desert", clicked: false},
+    {text: "You have had a close call with death", clicked: false}
   ]
 
   const [stateQuestions, setStateQuestions] = useState(questions)
+  const [isQuizSubmitted, setIsQuizSubmitted] = useState(false)
 
-  function render() {
-    console.log(questions)
-    
+  function SubmitButton() {
+    return (
+      <div className="submitContainer"> 
+        <button className="submitButton" onClick={() => {
+          setIsQuizSubmitted(true)
+        }}>
+          <span className="submitText">
+            Find out your monster!
+          </span>
+        </button>
+      </div>
+    )
+  }
+
+  function MonsterOutput() {
+    return (
+      <div>
+        
+      </div>
+    )
   }
 
   return (
@@ -40,6 +75,10 @@ function App() {
           })
         }
       </div>
+
+      {
+        isQuizSubmitted ? null : <SubmitButton />
+      }
     </div>
   )
 }
