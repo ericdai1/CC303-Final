89ef544d5efc1d9b9fefdc1b6df24e9846f50aec
Eric Dai <ericdai123@gmail.com>
2022-12-04 21:46:45 -0600

    Finished adding description and images for monsters.

--- src/App.js
+++ src/App.js
@@ -18,7 +18,44 @@ import Pegasus from "./Monsters/Pegasus.jfif"
 
 function App() {
   const monsterWeights = { "Cerberus": 0, "Medusa": 0, "Sphinx": 0, "Siren": 0, "Harpy": 0, "Hydra": 0, "Fury": 0,
-      "Chimera": 0, "Griffin": 0, "Cyclops": 0, "Minotaur": 0, "Centaur": 0, "Pegasus": 0}
+    "Chimera": 0, "Griffin": 0, "Cyclops": 0, "Minotaur": 0, "Centaur": 0, "Pegasus": 0
+  }
+  
+  const images = {
+    "Cerberus": Cerberus, "Medusa": Medusa, "Sphinx": Sphinx, "Siren": Siren, "Harpy": Harpy,
+    "Hydra": Hydra, "Fury": Fury, "Chimera": Chimera, "Griffin": Griffin, "Cyclops": Cyclops,
+    "Minotaur": Minotaur, "Centaur": Centaur, "Pegasus": Pegasus
+  }
+
+  const descriptions = {
+    "Cerberus": "You got Cerberus! The three-headed guard dog of the underworld, " + 
+      "putting fear into anyone who enters, except for Hercules :) ",
+    "Medusa": "You got Medusa! The gorgon sister with terrifying snake hair and deadly eyes, " +
+      "turning all who stare to stone, except for gods. Just beware of Percy Jackson...",
+    "Sphinx": "You got Sphinx! The mythical creature with the head of a human, " +
+      "the body of a lion, and the wings of a falcon. You eat those who cannot answer your tricky riddles. ",
+    "Siren": "You got Siren! A creature who tempts the sailors trekking dangerous seas, killing any " +
+      "that hear your beautiful but deadly song. Sadly, you could not outsmart Odysseus :(",
+    "Harpy": "You got Harpy! A creature who is a terrifying mix of human and eagle that travels at speeds " +
+      "the human eye cannot see, being confused for winds that carried people away in the Odyssey.",
+    "Hydra": "You got Hydra! The strongest creature in the seas, and known for the ability to regrow multiple " +
+      "heads when one is decapitated. However, you should still be wary of Hercules...",
+    "Fury": "You got Fury! One of the furies, who are goddesses so powerful and vengeful with the ability to cast " +
+      "curses on those that dare to commit crimes. This does, of course, only apply to the mortal beings of the world, not gods.",
+    "Chimera": "You got Chimera! An abomination of multiple different creatures, most commonly a lion, goat, and snake. " +
+      "You put fear into humans, but you should watch out for Bellerophen and his Pegasus.",
+    "Griffin": "You got Griffin! You are a famous creature with the body, tail, and legs of a lion and the head and wings " +
+      "of an eagle. You are well known for being the guardian of various treasures and goodies.",
+    "Cyclops": "You got Cyclops! You are the son of Poseidon himself, twice the size of mere mortals, and bolster one eye! " +
+      "But don’t get too arrogant: beware of cunning heroes like Odysseus...",
+    "Minotaur": "You got Minotaur! You are a monster with the body of a man and the head and tail of a bull. Unfortunately, " +
+      "you are imprisoned in the famous maze, the Labyrinth. Most fear you, but I’d stay wary of Theseus.",
+    "Centaur": "You got a Centaur! You are a vicious warrior with the upper body of a human and the lower legs of the horse. " +
+      "Although you are a force to be reckoned with in battle, I’d steer clear of Theseus and Hercules!",
+    "Pegasus": "You got a Pegasus! You are a beautiful, winged stallion sired by Poseidon himself! You are a very famous icon " +
+      "in Ancient Greece, depicted on several pieces of art even during the Renaissance."
+  }
+  
   const questionWeights = [
     { "Cerberus": 150 },
     { "Sphinx": 50, "Chimera": 50, "Griffin": 50 },
@@ -81,7 +118,7 @@ function App() {
     )
   }
 
-  function assignWeights() {
+  function getMonster() {
     for (let i = 0; i < stateQuestions.length; i++) {
       if (stateQuestions.clicked) {
         let weightsToAdd = questionWeights[i];
@@ -90,10 +127,12 @@ function App() {
         }
       }
     }
+
+    return Object.keys(monsterWeights).reduce((a, b) => monsterWeights[a] > monsterWeights[b] ? a : b);
   }
 
   function MonsterOutput() {
-    assignWeights();
+    let mostCommonMonster = getMonster();
     return (
       <div className="monsterOutput">
         <img src={Cerberus} />
